77e93e732b638aa0bcb06b296d9e67399371fc9f
mnadea001 <mathilde.nadeau@hotmail.fr>
2023-12-09 19:50:47 +0100

    caroussel OK

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ npm create vite@latest [nom projet]
 cd [nom projet]
 yarn install -> installation des dépendances
 yarn run dev -> lancemenet du serveur de dev
+
+
+# Documentation 
+
+** Carousel / Slider
+https://reactjsexample.com/a-beautiful-scroll-triggered-carousel-with-react/
--- src/components/SliderSection.tsx
+++ src/components/SliderSection.tsx
@@ -1,16 +1,43 @@
-import './SliderSection.css'
+import React from 'react';
+import './SliderSection.css';
+import ScrollCarousel from 'scroll-carousel-react';
+import icon1 from '../assets/icon1.png';
+import icon2 from '../assets/icon2.png';
+import icon3 from '../assets/icon3.png';
+import icon4 from '../assets/icon4.png';
+import icon5 from '../assets/icon5.png';
+import icon6 from '../assets/icon6.png';
+import icon7 from '../assets/icon7.png';
+import icon8 from '../assets/icon8.png';
+import icon9 from '../assets/icon9.png';
+import icon10 from '../assets/icon10.png';
+
+const iconArray = [icon1, icon2, icon3, icon4, icon5, icon6, icon7, icon8, icon9, icon10];
 
 const SliderSection: React.FC = () => {
+  const itemArray = Array.from({ length: 10 }, (_, index) => index + 1); // Array [1, 2, 3, ..., 11]
+
   return (
-    <section className="slider-section">
-      <span className="d-flex">
-        {/* <img src="assets/FILE.png" height="40" alt="" /> */}
-        <h2>Compétences</h2>
-      </span>
-      <div className="slider">
-        {/* Your slider content goes here */}
-      </div>
-    </section>
+    <>
+      <h1>Mes compétences</h1>
+      <p>Voici les langages que je maitrise</p>
+      <ScrollCarousel
+        autoplay
+        autoplaySpeed={8}
+        smartSpeed='true'
+        onReady={() => console.log('I am ready')}
+      >
+        {itemArray.map((item) => (
+          <div key={item} className='slide'>
+            <img
+              src={iconArray[item - 1]}
+              alt={`Icon ${item}`}
+              className='icon-image'
+            />
+          </div>
+        ))}
+      </ScrollCarousel>
+    </>
   );
 };
 
