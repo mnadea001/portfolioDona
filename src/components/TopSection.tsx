1887c074906d7f0353c89e82af1d5829d9c8aae0
mnadea001 <mathilde.nadeau@hotmail.fr>
2023-12-09 18:52:21 +0100

    WIP

--- src/components/TopSection.tsx
+++ src/components/TopSection.tsx
@@ -1,48 +1,25 @@
 import './TopSection.css'
 import { useParallax } from "react-scroll-parallax";
 const TopSection: React.FC = () => {
-  // const scaleCParallax = useParallax({
-  //   scaleX: [0, 3, "easeInQuad"],
-  // });
-  // const parallaxRotateY = useParallax({
-  //   rotateY: [0, 360],
-  // });
-  // const parallaxRotateY2 = useParallax({
-  //   rotateY: [0, 360],
-  // });
-  // const parallaxRotateY3 = useParallax({
-  //   rotateY: [0, 360],
-  // });
   const parallaxEasing = useParallax({
     easing: "easeOutQuad",
-    translateX: [-340, 100],
+    translateX: [-300, 100],
+    translateY: [0, 100],
   });
   const parallaxEasingLeft = useParallax({
-    easing: [1, -0.75, 0.5, 1.34],
-    translateX: [0, -260],
-    translateY: [1100],
+    easing: "easeOutQuad",
+    translateX: [300, -100],
+    translateY: [0, 100],
   });
   return (
     <div>
       <section className="bg-container">
-        <img
-          // ref={parallaxRotateY.ref}
-          src="https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-        />
         <div className="absolute-text">
-          <h1 ref={parallaxEasing.ref}>MEMBERSHIP WEBSITE</h1>
-          <h2 ref={parallaxEasingLeft.ref}>For members only non profitable</h2>
+        <div className="colored-circle first-circle" ref={parallaxEasing.ref} />
+        <div className="colored-circle second-circle" ref={parallaxEasingLeft.ref} />
         </div>
       </section>
       <br />
-      {/* <section className="card-container" ref={scaleCParallax.ref}>
-        <div className="card">
-          <img src="https://images.pexels.com/photos/2422294/pexels-photo-2422294.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
-        </div>
-        <div className="card">
-          <img src="https://images.pexels.com/photos/2422290/pexels-photo-2422290.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
-        </div>
-      </section> */}
 
       <br />
       <br />
@@ -56,22 +33,6 @@ const TopSection: React.FC = () => {
       <br />      <br />
       <br />      <br />
       <br />
-      {/* <section className="card-container">
-        <div className="card" ref={parallaxRotateY2.ref}>
-          <img src="https://images.pexels.com/photos/3184398/pexels-photo-3184398.jpeg?auto=compress&cs=tinysrgb&w=800" />
-        </div>
-        <div ref={parallaxRotateY3.ref} className="card">
-          <img src="https://images.pexels.com/photos/1181438/pexels-photo-1181438.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
-        </div>
-      </section> */}
-      <br />
-
-      {/* <section className="subscribe">
-        <h1>Subscribe to our news letter</h1>
-        <br />
-        <input type="email" placeholder="youremail@gmail.com" />
-        <button>Subscribe</button>
-      </section> */}
     </div>
   );
 };
