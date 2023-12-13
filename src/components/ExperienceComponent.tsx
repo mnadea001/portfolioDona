d31107e763a8196d9b4b0de491973175d1214191
mnadea001 <mathilde.nadeau@hotmail.fr>
2023-12-13 19:09:42 +0100

    wip

--- src/components/ExperienceComponent.tsx
+++ src/components/ExperienceComponent.tsx
@@ -1,34 +1,41 @@
+import React from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import TransitionsModal from './TransitionsModal';
 
-export default function ExperienceComponent() {
+interface ExperienceComponentProps {
+  imgSrc: string;
+  title: string;
+  description: string;
+  skills: string[];
+}
 
+const ExperienceComponent: React.FC<ExperienceComponentProps> = ({ imgSrc, title, description, skills }) => {
   return (
     <Card sx={{ display: 'flex' }}>
       <Box sx={{ display: 'flex', flexDirection: 'column' }}>
         <CardContent sx={{ flex: '1 0 auto' }}>
           <Typography component="div" variant="h5">
-            Live From Space
+            {title}
           </Typography>
           <Typography variant="subtitle1" color="text.secondary" component="div">
-            Mac Miller
+            {description}
           </Typography>
-          <TransitionsModal/>
+          <Typography variant="subtitle2" color="text.secondary" component="div">
+            Skills: {skills.join(', ')}
+          </Typography>
         </CardContent>
-        <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
-
-        </Box>
       </Box>
       <CardMedia
         component="img"
         sx={{ width: 151 }}
-        src="https://images.unsplash.com/photo-1682686581660-3693f0c588d2?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Live from space album cover"
+        src={imgSrc}
+        alt={`Image for ${title}`}
       />
     </Card>
   );
-}
+};
+
+export default ExperienceComponent;
--- src/components/ExperiencesSection.tsx
+++ src/components/ExperiencesSection.tsx
@@ -3,6 +3,27 @@ import './ExperiencesSection.css';
 import { useParallax } from "react-scroll-parallax";
 import ExperienceComponent from './ExperienceComponent';
 
+const experiencesData = [
+  {
+    imgSrc: 'https://images.unsplash.com/photo-1682686580003-22d3d65399a8?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Experience 1',
+    description: 'Description of experience 1.',
+    skills: ['Skill 1', 'Skill 2', 'Skill 3'],
+  },
+  {
+    imgSrc: 'path-to-image-2.jpg',
+    title: 'Experience 2',
+    description: 'Description of experience 2.',
+    skills: ['Skill 4', 'Skill 5', 'Skill 6'],
+  },
+  {
+    imgSrc: 'path-to-image-3.jpg',
+    title: 'Experience 3',
+    description: 'Description of experience 3.',
+    skills: ['Skill 7', 'Skill 8', 'Skill 9'],
+  },
+  // Add more experiences as needed
+];
 const ExperiencesSection: React.FC = () => {
   const parallaxEasingFirst = useParallax({
     easing: "easeOutQuad",
@@ -21,22 +42,25 @@ const ExperiencesSection: React.FC = () => {
     translateY: [100, 0],
     speed: -20
   });
+
+  
+  
   return (
     <div>
       <h1>EXPERIENCES</h1>
       <section className="bg-container">
         <div className="absolute-text">
 
-          <div className="colored-circle img" ref={parallaxEasingFirst.ref}>
-            <ExperienceComponent />
-          </div>
-
-          <div className="colored-circle img" ref={parallaxEasingSecond.ref}>
-            <ExperienceComponent />
-          </div>
-          <div className="colored-circle img" ref={parallaxEasingThird.ref}>
-            <ExperienceComponent />
-          </div>
+        {experiencesData.map((experience, index) => (
+            <div className="colored-circle img" key={index} ref={index === 0 ? parallaxEasingFirst.ref : index === 1 ? parallaxEasingSecond.ref : parallaxEasingThird.ref}>
+              <ExperienceComponent
+                imgSrc={experience.imgSrc}
+                title={experience.title}
+                description={experience.description}
+                skills={experience.skills}
+              />
+            </div>
+          ))}
         </div>
       </section>
 
