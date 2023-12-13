e1186314f80501cc1cd5fda1a870846a8fdb06d0
mnadea001 <mathilde.nadeau@hotmail.fr>
2023-12-13 20:25:05 +0100

    WIP

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,22 @@
-import './Footer.css'; 
+import './Footer.css';
+import '../assets/MailIcon.tsx'
+import MailIcon from '../assets/MailIcon';
+import IconGithub from '../assets/IconGithub';
 
 const Footer: React.FC = () => {
   return (
     <footer className="footer-container">
       <div className="footer-content">
-        <p>&copy; 2023 Your Company</p>
         <ul className="footer-links">
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
+          <li>CONTACT</li>
+          <li><a href="/about">
+            <MailIcon />
+            Mail</a></li>
+          <li><a href="/contact">
+            <IconGithub/>
+            Github</a></li>
         </ul>
+        <p>Back to top</p>
       </div>
     </footer>
   );
