bf12eab18b6de07173cf02bca61d36aa581459fd
mnadea001 <mathilde.nadeau@hotmail.fr>
2023-12-13 23:10:27 +0100

    back to top function

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,33 @@
+
 import './Footer.css';
-import '../assets/MailIcon.tsx'
+import '../assets/MailIcon.tsx';
 import MailIcon from '../assets/MailIcon';
 import IconGithub from '../assets/IconGithub';
 
 const Footer: React.FC = () => {
+  const handleBackToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
     <footer className="footer-container">
       <div className="footer-content">
         <ul className="footer-links">
           <li>CONTACT</li>
-          <li><a href="/about">
-            <MailIcon />
-            Mail</a></li>
-          <li><a href="/contact">
-            <IconGithub/>
-            Github</a></li>
+          <li>
+            <a href="/about">
+              <MailIcon />
+              Mail
+            </a>
+          </li>
+          <li>
+            <a href="/contact">
+              <IconGithub />
+              Github
+            </a>
+          </li>
         </ul>
-        <p>Back to top</p>
+        <p onClick={handleBackToTop}>Back to top</p>
       </div>
     </footer>
   );
