6764614c38912b2c80a17cb4356727c794a83745
mnadea001 <mathilde.nadeau@hotmail.fr>
2023-12-22 22:34:13 +0100

    404 styley

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,40 +1,49 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { RenderTexture, PerspectiveCamera, Text, ContactShadows } from '@react-three/drei'
+import { JSX } from 'react/jsx-runtime'
+import { Points, PointMaterial } from '@react-three/drei'
+import * as random from 'maath/random/dist/maath-random.esm'
+import './NotFound.css';
+import Header from './Header'
 
 
-function Cube() {
-    const textRef = useRef()
-    useFrame((state) => (textRef.current.position.x = Math.sin(state.clock.elapsedTime) * 2))
+function Stars(props: JSX.IntrinsicAttributes) {
+    const ref = useRef()
+    const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.5 }))
+    useFrame((state, delta) => {
+      ref.current.rotation.x -= delta / 10
+      ref.current.rotation.y -= delta / 15
+    })
     return (
-        <mesh>
-            <boxGeometry />
-            <meshStandardMaterial>
-                <RenderTexture attach="map" anisotropy={16}>
-                    <PerspectiveCamera makeDefault manual aspect={1 / 1} position={[0, 0, 5]} />
-                    <color attach="background" args={['#1501FF']} />
-                    <ambientLight intensity={0.5} />
-                    <directionalLight position={[10, 10, 5]} />
-                    <Text ref={textRef} fontSize={4} color="#D5FF3C">
-                        404
-                    </Text>
-                </RenderTexture>
-            </meshStandardMaterial>
-        </mesh>
+      <group rotation={[0, 0, Math.PI / 4]}>
+        <Points ref={ref} positions={sphere} stride={3} frustumCulled={false} {...props}>
+          <PointMaterial transparent color="#ffa0e0" size={0.005} sizeAttenuation={true} depthWrite={false} />
+        </Points>
+      </group>
     )
-}
+  }
 
+function Overlay() {
+    return (
+      <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
+        <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate3d(-50%,-50%,0)' }}>
+          <h1 style={{ margin: 0, padding: 0, fontSize: '15em', fontWeight: 500, letterSpacing: '-0.05em' }}>404</h1>
+          <p>Page introuvable</p>
+        </div>
+        <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }}>retour à l'accueil —</div>
+      </div>
+    )
+  }
 const NotFound: React.FC = () => {
 
     return (
-        <>
-            <Canvas camera={{ position: [4, 4, 4], fov: 25 }}>
-                <ambientLight intensity={0.5} />
-                <Cube />
-                <ContactShadows frames={1} position={[0, -0.5, 0]} blur={1} opacity={0.75} />
-                <ContactShadows frames={1} position={[0, -0.5, 0]} blur={3} color="#1501FF" />
-            </Canvas>
-        </>
+        <div style={{ position: 'absolute', top: 0, left: 0,  width: '100%', height: '100%', background: '#12071f' }}>
+    <Canvas camera={{ position: [0, 0, 1] }}>
+      <Stars />
+    </Canvas>
+    <Overlay />
+    <Header />
+    </div>
     );
 };
 
