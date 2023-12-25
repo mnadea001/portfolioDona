ec9430a1eb3a07f44da1452badac2a827a66125a
mnadea001 <mathilde.nadeau@hotmail.fr>
2023-12-25 21:20:32 +0100

    motionpath svg

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,28 @@ import './Footer.css';
 import IconGithub from '../assets/icons/IconGithub';
 import MailIcon from '../assets/icons/MailIcon';
 import { useEffect } from 'react';
-import { gsap } from "gsap";
+import gsap from "gsap";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
+gsap.registerPlugin(MotionPathPlugin);
 const Footer: React.FC = () => {
   const handleBackToTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
+
+  gsap.to("#rect", {
+    duration: 5,
+    repeat: 100,
+    yoyo: true,
+    ease: "power1.inOut",
+    motionPath: {
+      path: "#path",
+      align: "#path",
+      autoRotate: true,
+      alignOrigin: [0.5, 0.5]
+    }
+  });
+
 
   useEffect(() => {
     gsap.set(".ball", { xPercent: 0, yPercent: 0 });
@@ -30,25 +46,30 @@ const Footer: React.FC = () => {
     <footer className="footer-container">
       <div className="ball"></div>
       <div className="ball"></div>
+      <svg width="100%" height="100%" viewBox="-20 0 557 190" id="svg">
+        <circle cx="100" cy="100" r="3" />
+        <circle cx="300" cy="20" r="3" />
+        <path id="path" d="M9,100c0,0,18.53-41.58,49.91-65.11c30-22.5,65.81-24.88,77.39-24.88c33.87,0,57.55,11.71,77.05,28.47c23.09,19.85,40.33,46.79,61.71,69.77c24.09,25.89,53.44,46.75,102.37,46.75c22.23,0,40.62-2.83,55.84-7.43c27.97-8.45,44.21-22.88,54.78-36.7c14.35-18.75,16.43-36.37,16.43-36.37" />
+        <g id="rect">
+          <rect width="85" height="30" fill="#1501FF" />
+          <text x="0" y="19" font-size="14">What's next ?</text>
+        </g>
+      </svg>
       <div className="footer-content">
         <ul className="footer-links">
-          <li>CONTACT</li>
           <li>
-            <MailIcon />
-            <a href="/about">
-              Mail
+            <a className="footer-link" href="/about">
+              MAIL
             </a>
           </li>
           <li>
-            <IconGithub />
-            <a href="/contact">
-              Github
+            <a className="footer-link" href="/contact">
+              GITHUB
             </a>
           </li>
         </ul>
-        <p onClick={handleBackToTop}>Back to top</p>
+        <button onClick={handleBackToTop}>Back to top</button>
       </div>
-
     </footer>
   );
 };
