9f9949b9f2902da815634b32fd12558a8f4c1017
mnadea001 <mathilde.nadeau@hotmail.fr>
2024-01-24 21:00:43 +0100

    WIP

--- src/components/AccordionItem.tsx
+++ src/components/AccordionItem.tsx
@@ -4,7 +4,18 @@ import IconDown from '../assets/icons/IconDown';
 import './AccordionItem.css';
 import { DarkModeContext } from '../providers/DarkModeProvider';
 import { useContext } from 'react';
-import Chip from '@mui/material/Chip';
+import Chip from '@mui/material/Chip';import Box from '@mui/material/Box';
+import MobileStepper from '@mui/material/MobileStepper';
+import Paper from '@mui/material/Paper';
+// import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+import { useTheme } from '@mui/material/styles';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 interface AccordionItemProps {
   title: string;
@@ -15,6 +26,29 @@ interface AccordionItemProps {
   number: number
 }
 
+const images = [
+  {
+    label: 'San Francisco – Oakland Bay Bridge, United States',
+    imgPath:
+      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+  {
+    label: 'Bird',
+    imgPath:
+      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+  {
+    label: 'Bali, Indonesia',
+    imgPath:
+      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
+  },
+  {
+    label: 'Goč, Serbia',
+    imgPath:
+      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+];
+
 const AccordionItem: React.FC<AccordionItemProps> = ({ title, content, imageSrc, description, languages, number }) => {
   const [isHovered, setIsHovered] = useState(false);
   const darkModeContext = useContext(DarkModeContext);
@@ -23,6 +57,22 @@ const AccordionItem: React.FC<AccordionItemProps> = ({ title, content, imageSrc,
     color: darkMode === true ? '#D5FF3C' : '#333333',
     fontWeight: 'bold'
 };
+const theme = useTheme();
+const [activeStep, setActiveStep] = useState(0);
+const maxSteps = images.length;
+
+const handleNext = () => {
+  setActiveStep((prevActiveStep) => prevActiveStep + 1);
+};
+
+const handleBack = () => {
+  setActiveStep((prevActiveStep) => prevActiveStep - 1);
+};
+
+const handleStepChange = (step: number) => {
+  setActiveStep(step);
+};
+
   return (
     <Accordion
       style={{ backgroundColor: 'transparent', border: 'none' }}
@@ -47,7 +97,9 @@ const AccordionItem: React.FC<AccordionItemProps> = ({ title, content, imageSrc,
 
       </AccordionSummary>
       <AccordionDetails style={{ backgroundColor: 'rgba(255, 255, 255, 0.2)',}}>
-        <Typography  style={textStyle}>{content}
+        <div className='d-flex'>
+<div className='d-block'>
+<Typography  style={textStyle}>{content}
         </Typography>
         <Typography>
           {description}
@@ -61,6 +113,81 @@ const AccordionItem: React.FC<AccordionItemProps> = ({ title, content, imageSrc,
             <Chip key={index} label={language} style={{ margin: '0.5em' }} />
           ))}
         </div>
+</div>
+<div className='d-block'>
+<Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+      <Paper
+        square
+        elevation={0}
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          height: 50,
+          pl: 2,
+          bgcolor: 'background.default',
+        }}
+      >
+        <Typography>{images[activeStep].label}</Typography>
+      </Paper>
+      <AutoPlaySwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+               {images.map((step, index) => (
+          <div key={step.label}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <Box
+                component="img"
+                sx={{
+                  height: 255,
+                  display: 'block',
+                  maxWidth: 400,
+                  overflow: 'hidden',
+                  width: '100%',
+                }}
+                src={step.imgPath}
+                alt={step.label}
+              />
+            ) : null}
+          </div>
+        ))}
+      </AutoPlaySwipeableViews>
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button
+            size="small"
+            onClick={handleNext}
+            disabled={activeStep === maxSteps - 1}
+          >
+            Next
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+            Back
+          </Button>
+        }
+      />
+    </Box>
+</div>
+</div>
+
+
       </AccordionDetails>
     </Accordion>
   );
--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -9,14 +9,17 @@ import runup from '../assets/images/runup.png';
 import takeme from '../assets/images/takeme.png';
 import chinesejava from '../assets/images/chinesejava.webp';
 import vdc from '../assets/images/vdc.png';
+import icon4 from '../assets/images/icon4.png';
+import icon7 from '../assets/images/icon7.png';
+import icon8 from '../assets/images/icon8.png';
 
 const projectsData = [
-  { title: 'Run Up Asso', content: 'Projet Symfony en binome', imageSrc: runup, description: 'Fonctionnalités: inscription/ connexion, CRUD profil associations, commentaires, favoris', languages: ['Symfony'], number: 2 },
-  { title: 'Séniorissime', content: 'Projet Symfony + API OpenWeather', imageSrc: senio, description: 'Affichage des activités en fonction de la météo d\'une ville', languages: ['Symfony', 'API', 'OpenWeather', 'Heroku'], number: 3 },
-  { title: 'Vinyl du Coin', content: 'Projet Symfony en autonomie', description: 'Fonctionnalités: inscription/ connexion, CRUD profil associations, commentaires, favoris, messagerie, filtres, pagination, barre de recherche, panel administrateur', imageSrc: vdc, languages: ['Symfony'], number: 1 },
-  { title: 'Chinese Year', content: 'Project Java Spring en autonomie', description: 'Fonctionnalités: affichage de proverbes aléatoires, définition du signe chinois en fonction de l\'age, réduction en fonction du prix du panier, liste des articles, panier d\'achat', imageSrc: chinesejava, languages: ['Java Spring', 'ThymeLeaf'], number: 2 },
-  { title: 'TakeMe', content: 'Project React Native', description: 'Affichage carte, détail annonce, formulaire de création d\'annonce', imageSrc: takeme, languages: ['React Native', 'Leaflet'], number: 3 },
-  { title: 'Beers Notation', content: 'Project React.JS', description: 'Fonctionnalités: inscription/ connexion, Liste / détail bière, commentaires, notation', imageSrc: beers, languages: ['React', 'FireBase', 'API', 'Vercel'], number: 1 },
+  { title: 'Run Up Asso', content: 'Projet Symfony en binome', imageSrc: icon8, description: 'Fonctionnalités: inscription/ connexion, CRUD profil associations, commentaires, favoris', languages: ['Symfony'], number: 2 },
+  { title: 'Séniorissime', content: 'Projet Symfony + API OpenWeather', imageSrc: icon8, description: 'Affichage des activités en fonction de la météo d\'une ville', languages: ['Symfony', 'API', 'OpenWeather', 'Heroku'], number: 3 },
+  { title: 'Vinyl du Coin', content: 'Projet Symfony en autonomie', description: 'Fonctionnalités: inscription/ connexion, CRUD profil associations, commentaires, favoris, messagerie, filtres, pagination, barre de recherche, panel administrateur', imageSrc: icon8, languages: ['Symfony'], number: 1 },
+  { title: 'Chinese Year', content: 'Project Java Spring en autonomie', description: 'Fonctionnalités: affichage de proverbes aléatoires, définition du signe chinois en fonction de l\'age, réduction en fonction du prix du panier, liste des articles, panier d\'achat', imageSrc: icon4, languages: ['Java Spring', 'ThymeLeaf'], number: 2 },
+  { title: 'TakeMe', content: 'Project React Native', description: 'Affichage carte, détail annonce, formulaire de création d\'annonce', imageSrc: icon7, languages: ['React Native', 'Leaflet'], number: 3 },
+  { title: 'Beers Notation', content: 'Project React.JS', description: 'Fonctionnalités: inscription/ connexion, Liste / détail bière, commentaires, notation', imageSrc: icon7, languages: ['React', 'FireBase', 'API', 'Vercel'], number: 1 },
 ];
 
 const ProjectsSection: React.FC = () => {
