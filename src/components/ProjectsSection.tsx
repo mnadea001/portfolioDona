9532ea9eb473ea9806c2340a1da7f4c86c0610af
mnadea001 <mathilde.nadeau@hotmail.fr>
2024-01-24 23:44:33 +0100

    WIP

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -13,14 +13,6 @@ import icon4 from '../assets/images/icon4.png';
 import icon7 from '../assets/images/icon7.png';
 import icon8 from '../assets/images/icon8.png';
 
-// const projectsData = [
-//   { title: 'Run Up Asso', content: 'Projet Symfony en binome', imageSrc: icon8, description: 'Fonctionnalités: inscription/ connexion, CRUD profil associations, commentaires, favoris', languages: ['Symfony'], number: 2 },
-//   { title: 'Séniorissime', content: 'Projet Symfony + API OpenWeather', imageSrc: icon8, description: 'Affichage des activités en fonction de la météo d\'une ville', languages: ['Symfony', 'API', 'OpenWeather', 'Heroku'], number: 3 },
-//   { title: 'Vinyl du Coin', content: 'Projet Symfony en autonomie', description: 'Fonctionnalités: inscription/ connexion, CRUD profil associations, commentaires, favoris, messagerie, filtres, pagination, barre de recherche, panel administrateur', imageSrc: icon8, languages: ['Symfony'], number: 1 },
-//   { title: 'Chinese Year', content: 'Project Java Spring en autonomie', description: 'Fonctionnalités: affichage de proverbes aléatoires, définition du signe chinois en fonction de l\'age, réduction en fonction du prix du panier, liste des articles, panier d\'achat', imageSrc: icon4, languages: ['Java Spring', 'ThymeLeaf'], number: 2 },
-//   { title: 'TakeMe', content: 'Project React Native', description: 'Affichage carte, détail annonce, formulaire de création d\'annonce', imageSrc: icon7, languages: ['React Native', 'Leaflet'], number: 3 },
-//   { title: 'Beers Notation', content: 'Project React.JS', description: 'Fonctionnalités: inscription/ connexion, Liste / détail bière, commentaires, notation', imageSrc: icon7, languages: ['React', 'FireBase', 'API', 'Vercel'], number: 1 },
-// ];
 
 const projectsData = [
   {
@@ -51,11 +43,83 @@ const projectsData = [
     images: [
       {
         label: 'Image 3',
-        imgPath: runup,
+        imgPath: senio,
       },
       {
         label: 'Image 4',
-        imgPath: icon8,
+        imgPath: senio,
+      },
+    ],
+  },
+  {
+    title: 'Vinyl du Coin',
+    content: 'Projet Symfony en autonomie',
+    description: 'Fonctionnalités: inscription/ connexion, CRUD profil associations, commentaires, favoris, messagerie, filtres, pagination, barre de recherche, panel administrateur',
+    languages: ['Symfony'],
+    number: 1,
+    imageSrc: icon8,
+    images: [
+      {
+        label: 'Image 3',
+        imgPath: vdc,
+      },
+      {
+        label: 'Image 4',
+        imgPath: vdc,
+      },
+    ],
+  },
+  {
+    title: 'Chinese Year',
+    content: 'Project Java Spring en autonomie',
+    description: 'Fonctionnalités: affichage de proverbes aléatoires, définition du signe chinois en fonction de l\'age, réduction en fonction du prix du panier, liste des articles, panier d\'achat',
+    languages: ['Java Spring', 'ThymeLeaf'],
+    number: 1,
+    imageSrc: icon4,
+    images: [
+      {
+        label: 'Image 3',
+        imgPath: chinesejava,
+      },
+      {
+        label: 'Image 4',
+        imgPath: chinesejava,
+      },
+    ],
+  },
+  {
+    title: 'TakeMe',
+    content: 'Project React Native',
+    description: 'Affichage carte, détail annonce, formulaire de création d\'annonce',
+    languages: ['React Native', 'Leaflet'],
+    number: 3,
+    imageSrc: icon7,
+    images: [
+      {
+        label: 'Image 3',
+        imgPath: takeme,
+      },
+      {
+        label: 'Image 4',
+        imgPath: takeme,
+      },
+    ],
+  },
+  {
+    title: 'Beers Notation',
+    content: 'Project React.JS',
+    description: 'Fonctionnalités: inscription/ connexion, Liste / détail bière, commentaires, notation',
+    languages: ['React', 'FireBase', 'API', 'Vercel'],
+    number: 3,
+    imageSrc: icon7,
+    images: [
+      {
+        label: 'Image 3',
+        imgPath: beers,
+      },
+      {
+        label: 'Image 4',
+        imgPath: beers,
       },
     ],
   }
