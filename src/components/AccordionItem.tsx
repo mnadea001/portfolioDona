7ff1585cc817e1585d28f9b2ab19d80de918c0ad
mnadea001 <mathilde.nadeau@hotmail.fr>
2024-01-24 22:38:50 +0100

    WIP

--- src/components/AccordionItem.tsx
+++ src/components/AccordionItem.tsx
@@ -4,7 +4,7 @@ import IconDown from '../assets/icons/IconDown';
 import './AccordionItem.css';
 import { DarkModeContext } from '../providers/DarkModeProvider';
 import { useContext } from 'react';
-import Chip from '@mui/material/Chip';import Box from '@mui/material/Box';
+import Chip from '@mui/material/Chip'; import Box from '@mui/material/Box';
 import MobileStepper from '@mui/material/MobileStepper';
 import Paper from '@mui/material/Paper';
 // import Typography from '@mui/material/Typography';
@@ -56,29 +56,29 @@ const AccordionItem: React.FC<AccordionItemProps> = ({ title, content, imageSrc,
   const textStyle: React.CSSProperties = {
     color: darkMode === true ? '#D5FF3C' : '#333333',
     fontWeight: 'bold'
-};
-const theme = useTheme();
-const [activeStep, setActiveStep] = useState(0);
-const maxSteps = images.length;
+  };
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = useState(0);
+  const maxSteps = images.length;
 
-const handleNext = () => {
-  setActiveStep((prevActiveStep) => prevActiveStep + 1);
-};
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
 
-const handleBack = () => {
-  setActiveStep((prevActiveStep) => prevActiveStep - 1);
-};
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
 
-const handleStepChange = (step: number) => {
-  setActiveStep(step);
-};
+  const handleStepChange = (step: number) => {
+    setActiveStep(step);
+  };
 
   return (
     <Accordion
       style={{ backgroundColor: 'transparent', border: 'none' }}
     >
       <AccordionSummary
-        expandIcon={<IconDown className={darkMode ? 'iconDown' : 'iconDowndark'}  />}
+        expandIcon={<IconDown className={darkMode ? 'iconDown' : 'iconDowndark'} />}
         aria-controls="panel1a-content"
         id="panel1a-header"
         onMouseEnter={() => setIsHovered(true)}
@@ -96,98 +96,79 @@ const handleStepChange = (step: number) => {
         )}
 
       </AccordionSummary>
-      <AccordionDetails style={{ backgroundColor: 'rgba(255, 255, 255, 0.2)',}}>
-        <div className='d-flex'>
-<div className='d-block'>
-<Typography  style={textStyle}>{content}
-        </Typography>
-        <Typography>
-          {description}
-        </Typography>
-        <Typography>
-          Taille équipe: {number}
-        </Typography>
-        <Typography>Skills:</Typography>
-        <div>
-          {languages.map((language, index) => (
-            <Chip key={index} label={language} style={{ margin: '0.5em' }} />
-          ))}
-        </div>
-</div>
-<div className='d-block'>
-<Box sx={{ maxWidth: 400, flexGrow: 1 }}>
-      <Paper
-        square
-        elevation={0}
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          height: 50,
-          pl: 2,
-          bgcolor: 'background.default',
-        }}
-      >
-        <Typography>{images[activeStep].label}</Typography>
-      </Paper>
-      <AutoPlaySwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={activeStep}
-        onChangeIndex={handleStepChange}
-        enableMouseEvents
-      >
-               {images.map((step, index) => (
-          <div key={step.label}>
-            {Math.abs(activeStep - index) <= 2 ? (
-              <Box
-                component="img"
-                sx={{
-                  height: 255,
-                  display: 'block',
-                  maxWidth: 400,
-                  overflow: 'hidden',
-                  width: '100%',
-                }}
-                src={step.imgPath}
-                alt={step.label}
-              />
-            ) : null}
+      <AccordionDetails style={{ backgroundColor: 'rgba(255, 255, 255, 0.2)', }}>
+        <div className="flex-container">
+          <div className="flex-item">
+            <Typography style={textStyle}>{content}</Typography>
+            <Typography>{description}</Typography>
+            <Typography>Taille équipe: {number}</Typography>
+            <Typography>Skills:</Typography>
+            <div>
+              {languages.map((language, index) => (
+                <Chip key={index} label={language} style={{ margin: '0.5em' }} />
+              ))}
+            </div>
           </div>
-        ))}
-      </AutoPlaySwipeableViews>
-      <MobileStepper
-        steps={maxSteps}
-        position="static"
-        activeStep={activeStep}
-        nextButton={
-          <Button
-            size="small"
-            onClick={handleNext}
-            disabled={activeStep === maxSteps - 1}
-          >
-            Next
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowLeft />
-            ) : (
-              <KeyboardArrowRight />
-            )}
-          </Button>
-        }
-        backButton={
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowRight />
-            ) : (
-              <KeyboardArrowLeft />
-            )}
-            Back
-          </Button>
-        }
-      />
-    </Box>
-</div>
-</div>
-
-
+          <div className="flex-item">
+            <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+              <Paper
+                square
+                elevation={0}
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  height: 50,
+                  pl: 2,
+                  bgcolor: 'background.default',
+                }}
+              >
+                <Typography>{images[activeStep].label}</Typography>
+              </Paper>
+              <AutoPlaySwipeableViews
+                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                index={activeStep}
+                onChangeIndex={handleStepChange}
+                enableMouseEvents
+              >
+                {images.map((step, index) => (
+                  <div key={step.label}>
+                    {Math.abs(activeStep - index) <= 2 ? (
+                      <Box
+                        component="img"
+                        sx={{
+                          height: 255,
+                          display: 'block',
+                          maxWidth: 400,
+                          overflow: 'hidden',
+                          width: '100%',
+                        }}
+                        src={step.imgPath}
+                        alt={step.label}
+                      />
+                    ) : null}
+                  </div>
+                ))}
+              </AutoPlaySwipeableViews>
+              <MobileStepper
+                steps={maxSteps}
+                position="static"
+                activeStep={activeStep}
+                nextButton={
+                  <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+                    Next
+                    {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+                  </Button>
+                }
+                backButton={
+                  <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                    {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+                    Back
+                  </Button>
+                }
+              />
+            </Box>
+          </div>
+        </div>
       </AccordionDetails>
     </Accordion>
   );
