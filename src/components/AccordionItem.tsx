2c1650515b2d288d8efe70cf426a6fb9d2dd2534
mnadea001 <mathilde.nadeau@hotmail.fr>
2024-01-24 23:58:12 +0100

    WIP

--- src/components/AccordionItem.tsx
+++ src/components/AccordionItem.tsx
@@ -7,7 +7,6 @@ import { useContext } from 'react';
 import Chip from '@mui/material/Chip'; import Box from '@mui/material/Box';
 import MobileStepper from '@mui/material/MobileStepper';
 import Paper from '@mui/material/Paper';
-// import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
@@ -25,38 +24,6 @@ export interface AccordionItemProps {
   number: number;
   images: { label: string; imgPath: string }[];
 }
-// interface AccordionItemProps {
-//   title: string;
-//   content: string;
-//   imageSrc: string;
-//   description: string;
-//   languages: [string];
-//   number: number
-// }
-
-
-// const images = [
-//   {
-//     label: 'San Francisco – Oakland Bay Bridge, United States',
-//     imgPath:
-//       'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
-//   },
-//   {
-//     label: 'Bird',
-//     imgPath:
-//       'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
-//   },
-//   {
-//     label: 'Bali, Indonesia',
-//     imgPath:
-//       'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
-//   },
-//   {
-//     label: 'Goč, Serbia',
-//     imgPath:
-//       'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
-//   },
-// ];
 
 const AccordionItem: React.FC<AccordionItemProps> = ({ title, content, imageSrc, images, description, languages, number }) => {
   const [isHovered, setIsHovered] = useState(false);
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -6,7 +6,18 @@ import DialogContentText from '@mui/material/DialogContentText';
 import CloseIcon from '../assets/icons/CloseIcon';
 import IconButton from '@mui/material/IconButton';
 import './Modal.css';
-import Chip from '@mui/material/Chip';
+import { Box, Paper, Typography } from '@mui/material';
+import { useContext, useState } from 'react';
+import Chip from '@mui/material/Chip'; 
+import MobileStepper from '@mui/material/MobileStepper';
+import Button from '@mui/material/Button';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+import { useTheme } from '@mui/material/styles';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 interface ModalProps {
   open: boolean;
@@ -23,6 +34,24 @@ interface ModalProps {
 }
 
 const Modal: React.FC<ModalProps> = ({ open, onClose, title, description, skills, context, date, position, expImg, images }) => {
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = useState(0);
+  const maxSteps = images.length;
+  
+  
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step: number) => {
+    setActiveStep(step);
+  };
+
+
   return (
     <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
       <DialogTitle>
@@ -52,7 +81,64 @@ const Modal: React.FC<ModalProps> = ({ open, onClose, title, description, skills
           </div>
 
           <div className="flex-item">
-            <img src={expImg} alt={`Image for ${title}`} className="modal-image" />
+          <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+              <Paper
+                square
+                elevation={0}
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  height: 50,
+                  pl: 2,
+                  bgcolor: 'background.default',
+                }}
+              >
+                <Typography>{images[activeStep].label}</Typography>
+              </Paper>
+              <AutoPlaySwipeableViews
+                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                index={activeStep}
+                onChangeIndex={handleStepChange}
+                enableMouseEvents
+              >
+                {images.map((step, index) => (
+                  <div key={step.label}>
+                    {Math.abs(activeStep - index) <= 2 ? (
+                      <Box
+                        component="img"
+                        sx={{
+                          height: 255,
+                          display: 'block',
+                          maxWidth: 400,
+                          overflow: 'hidden',
+                          width: '100%',
+                        }}
+                        src={step.imgPath}
+                        alt={step.label}
+                      />
+                    ) : null}
+                  </div>
+                ))}
+              </AutoPlaySwipeableViews>
+              <MobileStepper
+                steps={maxSteps}
+                position="static"
+                activeStep={activeStep}
+                nextButton={
+                  <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+                    Next
+                    {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+                  </Button>
+                }
+                backButton={
+                  <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                    {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+                    Back
+                  </Button>
+                }
+              />
+            </Box>
+            {/* <img src={expImg} alt={`Image for ${title}`} className="modal-image" /> */}
           </div>
         </div>
 
