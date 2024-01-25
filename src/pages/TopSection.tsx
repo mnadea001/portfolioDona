49ea493ad41c2a7d61010723128571c544910acb
mnadea001 <mathilde.nadeau@hotmail.fr>
2024-01-25 22:30:20 +0100

    top section responsive

--- src/pages/TopSection.tsx
+++ src/pages/TopSection.tsx
@@ -6,30 +6,55 @@ import ExperiencesSection from '../components/ExperiencesSection';
 import DiplomaComponent from '../components/DiplomaComponent';
 import SliderSection from '../components/SliderSection';
 import { DarkModeContext } from '../providers/DarkModeProvider';
-import { useContext } from 'react';
+import { useEffect, useState, useContext } from 'react';
 
 const TopSection: React.FC = () => {
   const darkModeContext = useContext(DarkModeContext);
   const darkMode = darkModeContext?.darkMode || false;
 
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+    // Check if the screen width is below a certain threshold for mobile
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // Adjust the threshold as needed
+    };
+
+    // Initial check on component mount
+    handleResize();
+
+    // Listen for window resize events
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  // Use parallax settings based on screen size
   const parallaxEasing = useParallax({
     easing: "easeOutQuad",
-    translateX: [-300, 110],
-    translateY: [0, 100],
+    translateX: isMobile ? [-300, -100] : [-300, 110],
+    translateY: isMobile ? [0, 50] : [0, 100],
   });
+
   const parallaxEasingLeft = useParallax({
     easing: "easeOutQuad",
-    translateX: [300, -110],
-    translateY: [0, 100],
+    translateX: isMobile ? [-100, -300] : [300, -110],
+    translateY: isMobile ? [0, 50] : [0, 100],
   });
+
   const parallaxEasingTop = useParallax({
     easing: "easeOutQuad",
-    translateY: [30, 100],
+    translateX: isMobile ? [-200, -200] : '',
+    translateY: isMobile ? [10, 70] : [30, 100],
   });
+
   const parallax = useParallax<HTMLDivElement>({
     easing: "ease",
-    translateX: [-300, 100],
-    translateY: [300, 100],
+    translateX: isMobile ? [-50, 50] : [-300, 100],
+    translateY: isMobile ? [150, 50] : [300, 100],
     rotate: [0, 360],
     speed: -20
   });
