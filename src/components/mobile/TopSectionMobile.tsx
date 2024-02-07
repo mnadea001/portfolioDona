b1d18533b2a353a4b0b30bcfe8d3f0e2de929b47
mnadea001 <mathilde.nadeau@hotmail.fr>
2024-02-07 23:16:44 +0100

    WIP

--- src/components/mobile/TopSectionMobile.tsx
+++ src/components/mobile/TopSectionMobile.tsx
@@ -1,26 +1,16 @@
 import { useParallax } from 'react-scroll-parallax';
 import logo from '../../assets/images/MN.png'
-import { useEffect, useState } from 'react';
 
 const TopSectionMobile: React.FC = () => {
 
-    const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth <= 768);
 
-    useEffect(() => {
-        const handleResize = () => {
-            setIsMobile(window.innerWidth <= 768);
-        };
-        handleResize();
-        window.addEventListener('resize', handleResize);
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
 
     const parallaxEasingTop = useParallax<HTMLDivElement>({
         easing: "easeOutQuad",
-        translateX: isMobile ? [-200, -200] : [0, 0],
-        translateY: isMobile ? [10, 70] : [30, 100],
+        translateX: [-200, -200] ,
+        translateY: [70, 10] ,
+        // translateX: isMobile ? [-200, -200] : [0, 0],
+        // translateY: isMobile ? [10, 70] : [30, 100],
     });
 
     return (
