5cb1284b54d67234b9d5afe4a24a1c04921785b2
mnadea001 <mathilde.nadeau@hotmail.fr>
2024-02-07 23:27:47 +0100

    WIP

--- src/components/mobile/TopSectionMobile.tsx
+++ src/components/mobile/TopSectionMobile.tsx
@@ -1,23 +1,13 @@
-import { useParallax } from 'react-scroll-parallax';
 import logo from '../../assets/images/MN.png'
+import './TopSectionMobile.css';
 
 const TopSectionMobile: React.FC = () => {
 
-
-
-    const parallaxEasingTop = useParallax<HTMLDivElement>({
-        easing: "easeOutQuad",
-        translateX: [0, 0] ,
-        translateY: [70, 10] ,
-        // translateX: isMobile ? [-200, -200] : [0, 0],
-        // translateY: isMobile ? [10, 70] : [30, 100],
-    });
-
     return (
-        <section className="bg-container">
-               <div className="absolute-text">
-                <img ref={parallaxEasingTop.ref as React.RefObject<HTMLImageElement>} id="homeImg" src={logo} />
-                </div>
+        <section className="container-top">
+            <div className="div-img">
+                <img className="top-img" src={logo} />
+            </div>
         </section>
     );
 };
