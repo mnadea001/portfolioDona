ebe642d063c252cc79b578dcfb405eb64e4e21e0
mnadea001 <mathilde.nadeau@hotmail.fr>
2024-02-13 17:17:07 +0100

    WIP

--- src/components/mobile/DiplomaMobileComponent.tsx
+++ src/components/mobile/DiplomaMobileComponent.tsx
@@ -43,6 +43,18 @@ const DiplomaMobileComponent: React.FC = () => {
                             <div ref={MasterDev.ref} className="bubble-style" data-text="MASTER EXPERT DEV WEB (2025)" />
                     </section>
                 </div>
+                <div className="m-scroll">
+                    <div className="m-scroll__title">
+                        <div>
+                            <p>
+                                <a href="#">Licence</a> <a href="#">Economie Gestion</a> &amp; <a href="#">Master </a>  <a href="#">Management International </a>&amp; &nbsp;
+                            </p>
+                            {/* <p>
+                                <a href="#">Licence</a> <a href="#">Economie Gestion</a> &amp; <a href="#">Master </a> <a href="#">Management International </a>&amp; &nbsp;
+                            </p> */}
+                        </div>
+                    </div>
+                </div>
             </section>
         </>
     );
