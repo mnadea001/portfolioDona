f1af0af5e527824a66bee18736d49558a5700398
Mathilde NADEAU <mathilde.nadeau@hotmail.fr>
2024-09-16 22:40:25 +0200

    wip

--- src/assets/data/projects2024.js
+++ src/assets/data/projects2024.js
@@ -11,32 +11,7 @@ const projects2024 = [
         lieu: "Villars-sur-Ollon, Suisse",
         imageSrc: rootsfoundation
       },
-    // {
-    //   title: 'ROOTS FOUNDATION FEST',
-    //   date: '5 - 6 Janvier 2024',
-    //   lieu: "Villars-sur-Ollon, Suisse",
-    //   artistes: ['Earl16', 'Winston McNuff', 'Kibir La Amlak', 'Matic Horns', 'Aba Ariginal'], 
-    //   link: 'https://yogi-app.vercel.app/',
-    //   imageSrc: icon7,
-    //   images: [
-    //     {
-    //       label: 'Image 3',
-    //       imgPath: yogaone
-    //     },
-    //     {
-    //       label: 'Image 3',
-    //       imgPath: yogatwo
-    //     },
-    //     {
-    //       label: 'Image 3',
-    //       imgPath: yogathree
-    //     },
-    //     {
-    //       label: 'Image 3',
-    //       imgPath: yogafour
-    //     }
-    //   ]
-    // }
+ 
   ]
 
 
