3a5f5786639b4b29481d01ed90d9ce62b51607f0
Mathilde NADEAU <mathilde.nadeau@hotmail.fr>
2024-10-22 22:44:25 +0200

    fix mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,20 @@
-import { useState } from 'react';
-import { Outlet } from 'react-router';
-import { NavLink } from 'react-router-dom';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import './Header.css';
-import { Dialog, DialogContent } from '@mui/material';
+import { useState } from 'react'
+import { Outlet } from 'react-router'
+import { NavLink } from 'react-router-dom'
+import IconButton from '@mui/material/IconButton'
+import List from '@mui/material/List'
+import './Header.css'
+import { Dialog, DialogContent } from '@mui/material'
 
-
-import { Zoom, Bounce } from "react-awesome-reveal";
-import IconMenu from '../assets/icons/IconMenu';
+import { Zoom, Bounce } from 'react-awesome-reveal'
+import IconMenu from '../assets/icons/IconMenu'
 
 const Header: React.FC = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false)
   // const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth <= 768);
   const toggleModal = () => {
-    setIsModalOpen(!isModalOpen);
-  };
+    setIsModalOpen(!isModalOpen)
+  }
 
   // useEffect(() => {
   //   const handleResize = () => {
@@ -31,57 +30,48 @@ const Header: React.FC = () => {
   return (
     <>
       <div className="icons">
-
-        <h3 className='header-title'>MATILDA DONA</h3>
-        <IconButton onClick={toggleModal} >
-          <IconMenu className='menu-dark' />
+        <h3 className="header-title">MATILDA DONA</h3>
+        <IconButton onClick={toggleModal}>
+          <IconMenu className="menu-dark" />
         </IconButton>
       </div>
-      <Dialog open={isModalOpen} onClose={toggleModal} fullScreen >
+      <Dialog open={isModalOpen} onClose={toggleModal} fullScreen>
         <span
+          className="span-dialog"
           style={{
             display: 'flex',
             justifyContent: 'center',
-            backgroundColor: '#333333',
-            color: '#fff',
+            color: 'black',
             paddingTop: '50px'
-          }}
-        >
-          <IconButton
-            edge="start"
-            color="inherit"
-            onClick={toggleModal}
-            aria-label="close"
-            className="close-btn"
-          > FERMER
-   
+          }}>
+          <IconButton edge="start" color="inherit" onClick={toggleModal} aria-label="close" className="close-btn">
+            {' '}
+            FERMER
           </IconButton>
         </span>
-        <DialogContent sx={{   backgroundColor: '#333333' }}>
-           <List style={{ display: 'flex', textAlign: 'center', flexDirection: 'column' }}>
-        <Zoom>
-          <NavLink to="/" className="selected-dark" onClick={toggleModal}>
-            Accueil
-          </NavLink>
-        </Zoom>
-        {/* <Bounce>
+        <DialogContent className="span-dialog">
+          <List style={{ display: 'flex', textAlign: 'center', flexDirection: 'column' }}>
+            <Zoom>
+              <NavLink to="/" className="selected-dark" onClick={toggleModal}>
+                Accueil
+              </NavLink>
+            </Zoom>
+            {/* <Bounce>
           <NavLink to="/about" className="selected-dark" onClick={toggleModal}>
             Bio
           </NavLink>
         </Bounce> */}
-        <Bounce>
-          <NavLink to="/projects" className="selected-dark" onClick={toggleModal}>
-            Dates
-          </NavLink>
-        </Bounce>
-
-
-      </List>
+            <Bounce>
+              <NavLink to="/projects" className="selected-dark" onClick={toggleModal}>
+                Dates
+              </NavLink>
+            </Bounce>
+          </List>
         </DialogContent>
       </Dialog>
       <Outlet />
     </>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
