cfea99463fab87a82bb92917a0a0697c07315d4b
Mathilde NADEAU <mathilde.nadeau@hotmail.fr>
2024-10-22 22:57:44 +0200

    responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,56 +6,47 @@ import IntroSection from '../components/IntroSection'
 import DatesSection from '../components/DatesSection'
 import SliderSection from '../components/SliderSection'
 
-// import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import BannerComponent from '../components/BannerComponent'
 // import ExperiencesSection from '../components/ExperiencesSection'
 
 const Home: React.FC = () => {
-  // const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth <= 768)
+  const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth <= 768)
 
-  // useEffect(() => {
-  //   const handleResize = () => {
-  //     setIsMobile(window.innerWidth <= 768)
-  //   }
-  //   handleResize()
-  //   window.addEventListener('resize', handleResize)
-  //   return () => {
-  //     window.removeEventListener('resize', handleResize)
-  //   }
-  // }, [])
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768)
+    }
+    handleResize()
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
   return (
     <>
-    {/* <div>
-      {isMobile ? (
-        <>
-                  <IntroSection />
+      <div>
+        {isMobile ? (
+          <>
+            <IntroSection />
 
-<AboutSection />
-<DatesSection />
-<SliderSection />
-<BannerComponent/>
-        </>
-      ) : (
-        <>
-          <IntroSection />
+            <AboutSection />
+            <DatesSection />
+            <SliderSection />
+            <BannerComponent />
+          </>
+        ) : (
+          <>
+            <IntroSection />
 
-          <AboutSection />
-          <DatesSection />
-          <SliderSection />
-          <BannerComponent/>
-        </>
-      )}
-    </div>
-     */}
-           <>
-          <IntroSection />
-
-          <AboutSection />
-          <DatesSection />
-          <SliderSection />
-          <BannerComponent/>
-        </>
+            <AboutSection />
+            <DatesSection />
+            <SliderSection />
+            <BannerComponent />
+          </>
+        )}
+      </div>
     </>
   )
 }
