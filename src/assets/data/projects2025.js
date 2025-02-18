8db6a5ca5508f04f325422e922e41eed0a92460a
Mathilde NADEAU <mathilde.nadeau@hotmail.fr>
2025-02-18 20:12:30 +0100

    new dates

--- src/assets/data/projects2025.js
+++ src/assets/data/projects2025.js
@@ -2,13 +2,36 @@ import bilbaobrutality from '../images/bilbao-brutality.png'
 import winterdubcamp from '../images/winterdubcamp.jpg'
 import lyonfev from '../images/lyonfev.jpg'
 import dafvalencia from '../images/dafvalencia.png'
+import pinata from '../images/PINATA.jpg'
+import abendigo from '../images/abendigo.jpg'
+import dubarena from '../images/dubarena.jpg'
+import hfd from '../images/HFD.jpg'
+
 
 const projects2025 = [
+  {
+    title: 'DUB ARENA',
+    date: '5 avril 2025',
+    lieu: 'Portet sur Garonne, France',
+    imageSrc: dubarena
+  },
   {
     title: 'DAF',
     date: '7-9 mars 2025',
     lieu: 'Valence, Espagne',
     imageSrc: dafvalencia
+  },
+  {
+    title: 'SUB SESSION',
+    date: '1 mars 2025',
+    lieu: 'La Boiserie de Mazan, France',
+    imageSrc: hfd
+  },
+  {
+    title: 'PINATA RADIO',
+    date: '19 février 2025',
+    lieu: 'Montpellier, France',
+    imageSrc: pinata
   },
   {
     title: 'WINTER DUB WINTER',
