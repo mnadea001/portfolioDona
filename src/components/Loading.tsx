e9566472af27732dd79b55aa43063869592b0e93
Mathilde NADEAU <mathilde.nadeau@hotmail.fr>
2025-04-07 16:47:35 +0200

    misc fix

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,12 +1,6 @@
-import { motion } from 'framer-motion'
+
 import DiskSection from './DiskSection'
-const LoaderVariant = {
-  initial: { left: '-100vw' },
-  animate: {
-    left: '0',
-    transition: { duration: 2.3, type: 'tween', ease: 'linear' }
-  }
-}
+
 const Loading: React.FC = () => {
   return (
       <div className="loader-container">
@@ -14,7 +8,6 @@ const Loading: React.FC = () => {
       
           <DiskSection />
           <p className="loader-text">chargement en cours</p>
-          <motion.div className="loader-bar3" variants={LoaderVariant} initial="initial" animate="animate"></motion.div>
         </div>
       </div>
   )
