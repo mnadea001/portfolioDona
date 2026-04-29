82dfe71d6055396d5415afa83a89f696cd728698
mnadea001 <mathilde.nadeau@hotmail.fr>
2026-04-29 20:00:10 +0200

    wip dates

--- src/assets/data/projectsnext2026.ts
+++ src/assets/data/projectsnext2026.ts
@@ -1,9 +1,10 @@
 
-import perigueux from '../images/perigueux.jpg';
-import dubtales from '../images/dub-tales.jpg';
+
 import dubcamp26 from '../images/dubcamp26.jpg';
 import touko26 from '../images/touko26.jpg';
 import fortdec from '../images/fort-dec.jpg';
+import ardeida from '../images/ardeida.jpg';
+import cabaret26 from '../images/cabaret26.jpeg';
 
 
 interface Project {
@@ -15,38 +16,37 @@ interface Project {
 
 const projectsnext2026: Project[] = [
 
-        {
-    title: 'Strictly Vibes',
-    date: '7 Mars 2026',
-    lieu: 'Périgueux, France',
-    imageSrc: perigueux
+  {
+    title: 'Ardeida Festival',
+    date: '1 Mai 2026',
+    lieu: 'Vilamartin de Valdeorras, Espagne',
+    imageSrc: ardeida
   },
-        {
-    title: 'Dub Tales',
-    date: '28 Mars 2026',
-    lieu: 'Zaragoza, Espagne',
-    imageSrc: dubtales
-  },
-          {
+  {
     title: 'Toukofest',
     date: '30 Mai 2026',
     lieu: 'St Aubin, France',
     imageSrc: touko26
   },
-        {
+  {
     title: 'Dub Camp',
     date: '11 Juillet 2026',
     lieu: 'Joué-sur-Erdre, France',
     imageSrc: dubcamp26
   },
 
-        {
+  {
     title: 'Fort Décibel',
     date: '7 Aout 2026',
     lieu: 'Cussac Fort Médoc, France',
     imageSrc: fortdec
   },
-
+  {
+    title: 'CABARET VERT',
+    date: '20-23 Aout 2026',
+    lieu: 'Charleville-Mézières, France',
+    imageSrc: cabaret26
+  },
 
 ];
 
